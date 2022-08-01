0174b1c01e527f8fb1300888c167ce35316ceb85
varabyeu <varabyeu.v@yandex.by>
2022-08-01 17:54:21 +0300

    Add comments

--- app.py
+++ app.py
@@ -6,28 +6,41 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Main page render function"""
     return render_template('index.html')
 
 
 @app.route("/post_item", methods=["POST", "GET"])
 def post_item():
+    """Function to get data from form and save it to database"""
+    # work with form
     if request.method == 'POST':
+        # get all data from form
         items = request.form.getlist('data_input')
+        # saving data from form with function from external module
         save_to_db(items)
+        # When we click the submit button, we show a message
         return jsonify('Good! We\'ve send it to db!')
 
 
 @app.route("/items", methods=["GET"])
 def items():
+    """Function to show all available data on the page"""
+    # get connection to the db from external module function
     connection = connect_to_db()
+    # use try...except to ignore errors related to empty db
     try:
+        # get the cursor
         cursor = connection.cursor()
+        # Get the data from table
         cursor.execute("SELECT id, info FROM items")
         data = cursor.fetchall()
         cursor.close()
         connection.close()
+        # if all is OK, template with data is rendered
         return render_template('data_list.html', data=data)
     except ValueError:
+        # if we find a problem, template without data is rendered
         return render_template('empty_page.html')
 
 
--- database.py
+++ database.py
@@ -9,34 +9,42 @@ HOST = 'localhost'
 
 
 def connect_to_db():
+    """function to create a connection to db"""
     connection = psycopg2.connect(
         host=HOST,
         database=DATABASE,
         user=USER,
         password=PASSWORD
     )
+    # We call the table creation function when the conn is established
     create_table(connection)
     return connection
 
 
 def create_table(connection):
+    """Function to create a table in database"""
     cursor = connection.cursor()
+    # if table isn't exist, we create a new one
     cursor.execute(f'CREATE TABLE IF NOT EXISTS items ('
                    f'id serial NOT NULL PRIMARY KEY, '
                    f'info json NOT NULL)')
-    table_created = True
     connection.commit()
     cursor.close()
-    return table_created
 
 
 def save_to_db(items):
+    """Saves data to database"""
     connection = connect_to_db()
     cursor = connection.cursor()
+    # making query
     insert_query = """INSERT INTO items (info) VALUES (%s);"""
+    # whe iterate over the getting data
     for item in items:
+        # print data in terminal
         print(item)
+        # creating json type data
         record = json.dumps(item)
+        # and insert data in db using insert query
         cursor.execute(insert_query, [record])
     connection.commit()
     cursor.close()
